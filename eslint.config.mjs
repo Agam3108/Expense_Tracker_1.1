f7e9ae3b42c9d10b70bb3816849b515d7d52c164
Agam <118785408+Agam3108@users.noreply.github.com>
2024-12-15 18:06:06 +0530

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,7 +7,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.config({
-    extends: ['next/core-web-vitals', 'next/typescript'],
+    extensions: ['next/core-web-vitals', 'next/typescript'],
     rules: {
       // Disable all TypeScript-related rules
       '@typescript-eslint/*': 'off',
